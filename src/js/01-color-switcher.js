62ef6c7137f4f834ac16f697f31d6fcea50b05f1
Artem Makhov <makhovartem23@gmail.com>
2022-06-19 19:31:34 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
+
+let timerId = null;
+let isActive = false;
+
+
+startBtn.addEventListener('click', () => {
+    if (isActive) {
+        return;
+    }
+    isActive = true;
+    timerId = setInterval(() => {
+      
+      changeBackgroundColor()  
+    }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    isActive = false;
+});
+
+
+function changeBackgroundColor() { 
+    body.style.backgroundColor = getRandomHexColor(); 
+};
