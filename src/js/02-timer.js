1547f4a46b4f5e41ad32d573fbb1bff62e5878f6
Artem Makhov <makhovartem23@gmail.com>
2022-06-23 15:01:20 +0300

    task-2

---
+++ src/js/02-timer.js
@@ -0,0 +1,86 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css"
+import "flatpickr/dist/themes/material_orange.css";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+
+
+const refs =  {
+  inputField: document.querySelector("#datetime-picker"),
+  startBtn: document.querySelector("button[data-start]"),
+  days: document.querySelector("[data-days]"),
+  hours: document.querySelector("[data-hours]"),
+  minutes: document.querySelector("[data-minutes]"),
+  seconds: document.querySelector("[data-seconds]"),
+};
+
+refs.startBtn.setAttribute("disabled", "true");
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose: function choosingDates(selectedDates) {
+      if (selectedDates[0] < options.defaultDate) {
+          Notify.failure('Please choose a date in the future!');
+      } else { refs.startBtn.removeAttribute('disabled'); };
+    
+  },
+};
+
+
+
+const calendar = flatpickr(refs.inputField, options);
+
+const timerCountdown = {
+  intervalId: null,
+  isActive: false,
+
+  start () {
+    if ( this.isActive ) {
+      return;
+    }
+    const countDownTime = calendar.selectedDates[0].getTime();
+    if ( countDownTime < new Date().getTime() ) {
+      return;
+    }
+    
+    this.isActive = true;
+    this.intervalId = setInterval (() => {
+      const currentTime = Date.now();
+      const timeLeftMs = countDownTime - currentTime;
+      const timeConverted = convertMs(timeLeftMs);
+      updateClockFace(timeConverted);
+      if ( (timeLeftMs / 1000).toFixed(0) <= 0 ) {
+        clearInterval(this.intervalId);
+      }
+    }, 1000);
+  }
+}
+
+refs.startBtn.addEventListener("click", timerCountdown.start );
+
+function updateClockFace ({days, hours, minutes, seconds}) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.minutes.textContent = minutes;
+  refs.seconds.textContent = seconds;
+}
+
+function addLeadingZero (value) {
+  return String(value).padStart(2, "0");
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
